69be63c95a8fa846d55cf08ac86adb0d55bc60a2
Void <voidvenommc@gmail.com>
2021-01-31 02:29:15 -0500

    Update!
    Such wow!

---
+++ Assets/CustomCheatsUI/README.txt
@@ -0,0 +1,5 @@
+EasyCheat UI v1.1
+------------------
+Easily add a cheat menu to your game with EasyCheat UI! Easily connect cheat functions in scripts to a clickable button or toggleable toggle; no more typing secret codes to execute cheats just because you didn't want to take the time to make a pretty menu.
+
+Adding new buttons and toggles is easy. Just add the prefab into the UI's heirarchy, adjust it's position and label (a child in the prefab), and connect it to the scripted function(s) you want to call. 
---
+++ Assets/CustomCheatsUI/UI Assets/CheatsUIManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Cheats {
+    public class CheatsUIManager : MonoBehaviour {
+
+        [Header("Folding Customization")]
+        [SerializeField] private float foldSpeed = 1f;
+        [SerializeField] private float edgePadding = 10f;
+        [SerializeField] private bool lockViewOnOpen = false;
+
+        [SerializeField] private RectTransform cheatUI;
+        public KeyCode toggleKey = KeyCode.F10;
+
+        [HideInInspector] public bool showCheatsUI;
+
+        void Update() {
+            // handles state toggle
+            showCheatsUI = Input.GetKeyUp(toggleKey) ? !showCheatsUI : showCheatsUI;
+            if (lockViewOnOpen) SetViewLock(showCheatsUI);
+
+            // lerp towards appropriate position based on the new state of showCheatsUI
+            float halfWidth = cheatUI.rect.width / 2f;
+            float targetPos = showCheatsUI ? halfWidth + edgePadding : -halfWidth - edgePadding;
+            float lerpDelta = Time.deltaTime * foldSpeed * 5f;
+            
+            cheatUI.anchoredPosition = new Vector2(Mathf.Lerp(cheatUI.anchoredPosition.x, targetPos, lerpDelta), cheatUI.localPosition.y);
+        }
+
+        // Called by the close button
+        public void close() {
+            showCheatsUI = false;
+        }
+
+        // Called to inform the game of whether or not to lock the camera and free the mouse.
+        private void SetViewLock(bool b) {
+            throw new System.NotImplementedException("You still need to implement view locking!");
+        }
+
+        // Called by cheats that should invalidate the player's score.
+        public void InvalidateScore() {
+            throw new System.NotImplementedException("You still need to implement the invalidation of score!");
+        }
+    }
+}
