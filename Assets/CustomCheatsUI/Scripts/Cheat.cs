b29708402dc66304513e3b399582b104f6ac5090
Void <voidvenommc@gmail.com>
2021-01-31 02:33:50 -0500

    Update again!?
    Incredible wow!

--- Assets/CustomCheatsUI/Scripts/Cheat.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-namespace Cheats {
-    public class Cheat : MonoBehaviour {
-        public void InvalidateScore () {
-            throw new System.NotImplementedException("You still need to implement the invalidation of score!");
-        }
-    }
-}
